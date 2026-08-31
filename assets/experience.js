1dec915457862ad9f2d00de29a9edad0b34a6b91
librarp-stack <pr@librarp.com>
2026-08-31 13:10:59 +0530

    Your descriptive commit message

---
+++ assets/experience.js
@@ -0,0 +1,32 @@
+const slides = [
+    {
+        title: "CUSTOM VEHICLES",
+        bg: "../assets/images/hero/hero-3.webp",
+        fg: "../assets/images/vehicles/vehicles.webp"
+    },
+    {
+        title: "LUXURY LIVING",
+        bg: "../assets/images/hero/hero-4.webp",
+        fg: "../assets/images/housing/mansion.webp"
+    },
+    {
+        title: "PLAYER BUSINESSES",
+        bg: "../assets/images/hero/hero-5.webp",
+        fg: "../assets/images/businesses/business.webp"
+    },
+    {
+        title: "LIBRAHUB",
+        bg: "../assets/images/hero/hero-6.webp",
+        fg: "../assets/images/librahub/phone.webp"
+    },
+    {
+        title: "CASINO",
+        bg: "../assets/images/casino/casino.webp",
+        fg: "../assets/images/casino/casino.webp"
+    },
+    {
+        title: "REAL ECONOMY",
+        bg: "../assets/images/businesses/business.webp",
+        fg: "../assets/images/businesses/business.webp"
+    }
+];
